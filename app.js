5ea5b1b62c40b0ed1981f7ec368ba87c523c2a17
Tyreek Brasfield <tyreekbrasfield@gmail.com>
2024-08-25 22:52:05 -0500

    User can now loop through slider

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("click", e => {
     let arrows
     if (e.target.matches('.arrows')){
@@ -11,15 +12,30 @@ document.addEventListener("click", e => {
     }  
 })
 
-function onArrowClick(arrows){
-    const slider = arrows.closest('.top-movies-container').querySelector('.movie-slider')
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
-    if(arrows.classList.contains('left-arrow')){
-        slider.style.setProperty('--slider-index', sliderIndex - 1)
+function onArrowClick(arrows) {
+    const slider = arrows.closest('.top-movies-container').querySelector('.movie-slider');
+    let sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'));
+
+    const imgCount = slider.children.length; // Number of images in the slider
+    const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue('--items-per-screen'));
+
+    const maxIndex = Math.ceil(imgCount / itemsPerScreen); // Maximum index, based on the number of images
+
+    if (arrows.classList.contains('left-arrow')) {
+        if (sliderIndex - 1 < 0 ) {
+            // If at the beginning, move to the last item
+            slider.style.setProperty('--slider-index', maxIndex - 1);
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex - 1);
+        }
     }
 
-    if(arrows.classList.contains('right-arrow')){
-        slider.style.setProperty('--slider-index', sliderIndex + 1)
+    if (arrows.classList.contains('right-arrow')) {
+        if (sliderIndex + 1 >= maxIndex) {
+            // If at the end, move to the first item
+            slider.style.setProperty('--slider-index', 0);
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex + 1);
+        }
     }
-    console.log(slider)
 }
