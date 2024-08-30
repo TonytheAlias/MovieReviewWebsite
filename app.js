0033d1489c2d64da6372c893c9d55176eb72e28f
Tyreek Brasfield <tyreekbrasfield@gmail.com>
2024-08-30 12:57:24 -0500

    Finished For You slider and also added latest articles and community section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("click", e => {
     let arrows
+
     if (e.target.matches('.arrows')){
         arrows = e.target
     }
@@ -9,7 +10,20 @@ document.addEventListener("click", e => {
     }
     if(arrows != null) {
         onArrowClick(arrows)
-    }  
+    } 
+
+    
+    let otherArrows
+    
+    if (e.target.matches('.for-you-arrows')){
+        otherArrows = e.target
+    }
+    else {
+      otherArrows = e.target.closest('.for-you-arrows')
+    }
+    if(otherArrows != null){
+        onOtherArrowClick(otherArrows)
+    } 
 })
 
 function onArrowClick(arrows) {
@@ -31,6 +45,33 @@ function onArrowClick(arrows) {
     }
 
     if (arrows.classList.contains('right-arrow')) {
+        if (sliderIndex + 1 >= maxIndex) {
+            // If at the end, move to the first item
+            slider.style.setProperty('--slider-index', 0);
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex + 1);
+        }
+    }
+}
+function onOtherArrowClick(otherArrows) {
+    const slider = otherArrows.closest('.for-you-slider-container').querySelector('.for-you-slider');
+    let sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'));
+
+    const imgCount = slider.children.length; // Number of images in the slider
+    const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue('--items-per-screen'));
+
+    const maxIndex = Math.ceil(imgCount / itemsPerScreen); // Maximum index, based on the number of images
+
+    if (otherArrows.classList.contains('left-arrow')) {
+        if (sliderIndex - 1 < 0 ) {
+            // If at the beginning, move to the last item
+            slider.style.setProperty('--slider-index', maxIndex - 1);
+        } else {
+            slider.style.setProperty('--slider-index', sliderIndex - 1);
+        }
+    }
+
+    if (otherArrows.classList.contains('right-arrow')) {
         if (sliderIndex + 1 >= maxIndex) {
             // If at the end, move to the first item
             slider.style.setProperty('--slider-index', 0);
